56ab2fde6d7353aee4e6c5413e61a1ca4fc5ef76
Marcus <marcusbullock@hotmail.co.uk>
2016-04-04 15:28:16 +0100

    Pathetic attempt to refactor

--- lib/statement.rb
+++ lib/statement.rb
@@ -3,16 +3,19 @@ class Statement
   def print_statement(account)
     answer = ["date || credit || debit || balance"]
     account.history.reverse_each do |transaction|
-      formatter(transaction)
-      answer.push("#{transaction[:date]} || #{'%.02f' % @result_credit if @result_credit} || #{'%.02f' % @result_debit if @result_debit} || #{'%.02f' % transaction[:balance]}")
+      set_credit_and_debit(transaction)
+      answer.push("#{transaction[:date]} || #{money_format(@result_credit)} || #{money_format(@result_debit)} || #{money_format(transaction[:balance])}")
     end
     answer.each_slice(1) { |element| puts element.join}
   end
 
-  def formatter(hash)
+  def set_credit_and_debit(hash)
     @result_credit = hash[:sum] if hash[:sum] > 0
     @result_debit = hash[:sum] if hash[:sum] < 0
   end
 
+  def money_format(sum)
+    '%.02f' % sum if sum
+  end
 
 end
