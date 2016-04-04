3643f98f111051335ff46d9e13d3e60747960ca2
Marcus <marcusbullock@hotmail.co.uk>
2016-04-04 15:21:56 +0100

    Really awful MVP achieved

--- lib/statement.rb
+++ lib/statement.rb
@@ -4,19 +4,15 @@ class Statement
     answer = ["date || credit || debit || balance"]
     account.history.reverse_each do |transaction|
       formatter(transaction)
-      answer.push("#{transaction[:date]} || #{@result_credit} || #{@result_debit} || #{transaction[:balance]}"
+      answer.push("#{transaction[:date]} || #{'%.02f' % @result_credit if @result_credit} || #{'%.02f' % @result_debit if @result_debit} || #{'%.02f' % transaction[:balance]}")
     end
-    return answer
+    answer.each_slice(1) { |element| puts element.join}
   end
 
   def formatter(hash)
     @result_credit = hash[:sum] if hash[:sum] > 0
     @result_debit = hash[:sum] if hash[:sum] < 0
   end
-
-
-
-
 
 
 end
